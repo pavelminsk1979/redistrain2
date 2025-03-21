f0a665e7f78e9f1d2f67dbda0ce56bd9ecb8d97c
pavvelpotapov <pavelminsk1979@mail.ru>
2025-03-22 01:40:23 +0300

    commit1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,76 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: '127.0.0.1',
+      port: 6379,
+      username: 'newpavel1',
+      password: '7StP@55wrd', // Укажите ваш пароль
+      ttl: 600, // Время жизни кэша в секундах
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/*import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule } from '@nestjs/cache-manager';
+
+@Module({
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: '127.0.0.1', // Убедитесь, что это правильный хост
+      port: 6379, // Убедитесь, что это правильный порт
+      ttl: 600, // Время жизни кэша в секундах
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor() {
+    console.log('CacheModule has been registered with Redis.');
+  }
+}*/
+
+/*import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-redis-store';
+import { CacheModule } from '@nestjs/cache-manager';
+
+@Module({
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'localhost', // Убедитесь, что это правильный хост
+      port: 6379, // Убедитесь, что это правильный порт
+      ttl: 600, // Время жизни кэша в секундах
+      onClientReady: (client: any) => {
+        console.log('Initializing Redis client...');
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+        client.on('connect', () => {
+          console.log('Connected to Redis');
+        });
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+        client.on('error', (err) => {
+          console.error('Redis error:', err);
+        });
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}*/
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getHello() {
+    const key1 = 'key111';
+    console.log('Checking Redis connection...');
+    const value = await this.cacheManager.get(key1);
+    console.log('value', value);
+
+    if (value) return value;
+    const newValue = 'newHello World!';
+    await this.cacheManager.set(key1, newValue);
     return 'Hello World!';
   }
 }
